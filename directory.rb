6686f32a8510ab9e94c45ca5a712a84b070f165b
david anderson <d.a.m.anderson@gmail.com>
2016-09-18 20:07:00 +0100

    Interactive menu

--- directory.rb
+++ directory.rb
@@ -18,15 +18,55 @@ end
 
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+
+def print_first_letter(students,letter)
+  students.each_with_index do |student, i|
+    if student[:name][0].downcase == letter.downcase
+      puts "Names begining with #{letter} are..."
+      puts "#{i+1}.#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+def print_name_length(students,length)
+  students.each_with_index do |student, i|
+    if student[:name].length <= length
+      puts "Names shorter than #{length} characters are..."
+      puts "#{i+1}.#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+def interactive_menu
+students = []
+  loop do
+  # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+  # 2. read the input and save it into a variable
+    selection = gets.chomp
+  # 3. do what the user has asked
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit
+      else puts "I don't know what you mean, try again"
+    end
+  end
+end
+
 
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 #nothing happens until we call the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
